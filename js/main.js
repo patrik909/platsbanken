6ab2774257ab32eede6a94fc65c3977436df99f3
patrik909 <patrik.eriksson@medieinstitutet.se>
2018-04-11 14:49:35 +0200

    added classes to script file

--- js/main.js
+++ js/main.js
@@ -1,58 +1,78 @@
-listJobsByLanID(1)
-
-function listJobsByLanID(ID){
+class Fetch {
     
-    const fetchData = fetch(`http://api.arbetsformedlingen.se/af/v0/platsannonser/matchning?sida=1&antalrader=10&lanid=${ID}`);
-
-    fetchData.then((response) => {
-        return response.json();
-    }).then((fetchData) => {
-        displayLatestJobs(fetchData);
-        displayAmountOfJobs(fetchData)
-    }).catch((error) =>{
-        console.log(error);
-    })
-}
-
-function displayAmountOfJobs(jobs){
-    
-    const amountOfJobsDiv = document.getElementById('amountOfJobs');
-
-    let amountOfJobsContent = `<p> Antal jobb i ${jobs.matchningslista.matchningdata[0].kommunnamn}:  ${jobs.matchningslista.antal_platsannonser_exakta}`
-    
-    amountOfJobsDiv.innerHTML=amountOfJobsContent;
-
-}
-
-function displayLatestJobs(jobs){
-    
-    const jobListing = jobs.matchningslista.matchningdata
-    let listedJobs = "";
-    
-    for(let i = 0; i < jobListing.length; i++){
+    fetchLatestJobsByID(ID){
         
-        const date = jobListing[i].sista_ansokningsdag
-        let formatedDate = ""
-        if(date){
-            formatedDate = date.substring(0,10);
-        } else {
-            formatedDate = "Oklart";
-        }
+        const fetchLatestJobs = fetch(`http://api.arbetsformedlingen.se/af/v0/platsannonser/matchning?sida=1&antalrader=10&lanid=${ID}`);
         
-        listedJobs +=`
-            <div class="latestJobs">
-                <h3>${jobListing[i].annonsrubrik}</h3>
-                <p><span>${jobListing[i].yrkesbenamning}</span> - ${jobListing[i].kommunnamn}</p>
-                <p>${jobListing[i].arbetsplatsnamn}</p>
-                <p>${jobListing[i].anstallningstyp}</p>
-                <p><span>Sista ansökningsdag:</span> ${formatedDate}</p>
-                <button id="${jobListing[i].annonsid}"=>Läs mer!</button>
-            </div>
-        `;
-    
+        fetchLatestJobs.then((response) => {
+            return response.json();
+        }).then((fetchLatestJobs) => { 
+            newDOM.displayTotalAmoutOfJobs(fetchLatestJobs);
+            newDOM.displayLatestJobs(fetchLatestJobs);
+        }).catch((error) =>{     
+            console.log(error);
+       })
+        
     }
     
-    const outputListJobs = document.getElementById('outputListJobs');
-    outputListJobs.innerHTML=listedJobs
-    
 }
+
+class DOM {
+    
+    displayTotalAmoutOfJobs(jobs){
+
+        const amountOfJobsDiv = document.getElementById('amountOfJobs');
+        const lan = jobs.matchningslista.matchningdata[0].lan;
+        const amountOfJobs = jobs.matchningslista.antal_platsannonser_exakta;
+        
+        const amountOfJobsContent = `
+            <p> Antal jobb i ${lan}: ${amountOfJobs}
+        `;
+        
+        amountOfJobsDiv.innerHTML=amountOfJobsContent;
+        
+    }
+    
+    displayLatestJobs(jobs){
+        
+        const outputListJobs = document.getElementById('outputListJobs');
+        const jobData = jobs.matchningslista.matchningdata;
+        let listedJobs = "";
+        
+        for(let i = 0; i < jobData.length; i++){
+            
+            const date = jobData[i].sista_ansokningsdag
+            //Sending info to the contructor, which formates the data.
+            //newDOM.formateDate(jobData[i].sista_ansokningsdag)
+            
+            let formatedDate = ""
+            if(date){
+                formatedDate = date.substring(0,10);
+            } else {
+                formatedDate = "Oklart";
+            }
+            
+            listedJobs +=`
+                <div class="latestJobs">
+                    <h3>${jobData[i].annonsrubrik}</h3>
+                    <p><span>${jobData[i].yrkesbenamning}</span> - ${jobData[i].kommunnamn}</p>
+                    <p>${jobData[i].arbetsplatsnamn}</p>
+                    <p>${jobData[i].anstallningstyp}</p>
+                    <p><span>Sista ansökningsdag:</span> ${formatedDate}</p>
+                    <button id="${jobData[i].annonsid}"=>Läs mer!</button>
+                </div>
+            `;
+        }
+        outputListJobs.innerHTML=listedJobs
+    }
+    
+    formateDate(date){
+        console.log(date)
+    }
+       
+}
+
+//Starts fetch when entering the homepage
+const newDOM = new DOM;
+const newFetch = new Fetch;
+newFetch.fetchLatestJobsByID(1);
